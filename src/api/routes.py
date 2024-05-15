57d7ca8c312999c3e00cc4a6b975f412a3f35a5b
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-05-15 08:29:40 +0000

    Intallflask-jwt-extended

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,6 +6,12 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
@@ -17,6 +23,15 @@ def handle_hello():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/hello2', methods=['POST', 'GET'])
+def handle_hello2():
+
+    response_body = {
+        "message": "Microservicio Nuevo  en PRUEBA "
     }
 
     return jsonify(response_body), 200
